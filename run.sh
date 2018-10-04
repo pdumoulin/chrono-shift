b8c7889902dc7fe3e5f1bcdf555f072abfe342c4
Paul Dumoulin <paul.l.dumoulin@gmail.com>
2018-10-04 23:35:14 +0000

    remove log dir to simplify install

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # crontab setup
-# */5 * * * * /home/pi/game-time/run.sh > /home/pi/game-time/logs/cron.log 2>&1
+# */5 * * * * /home/pi/game-time/run.sh > /home/pi/game-time/cron.log 2>&1
 
 export PYTHONPATH=$PYTHONPATH:/home/pi/blinky
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
