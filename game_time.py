4d173e3ca78a4a186d6da69dd9858261a832d394
Paul Dumoulin <paul.l.dumoulin@gmail.com>
2020-03-29 19:36:22 +0000

    implement caching to reduce calls to nhl api

--- game_time.py
+++ game_time.py
@@ -5,11 +5,14 @@ import time
 
 from blinky import Wemo
 
+from decorators import cached
+
 import requests
 
 SWITCH = Wemo('192.168.1.81')
 TEAM = 'New York Rangers'
 WINDOW = 60
+CACHE_TTL = 3600
 
 
 def main():
@@ -17,21 +20,9 @@ def main():
     # did lights go on?
     ran = False
 
-    # load game data for the next month
-    url = 'https://statsapi.web.nhl.com/api/v1/schedule'
-    response = requests.get(url)
-    data = response.json()
-    if data['totalGames'] == 0:
-        print('No games today!')
-        exit()
-    games = data['dates'][0]['games']
     current_unix_time = int(time.time())
 
-    # filter out postponed games, thanks COVID-19
-    games = [
-        x for x in games
-        if x.get('status', {}).get('detailedState', '') != 'Postponed'
-    ]
+    games = get_schedule()
 
     # find if game just started
     for game in games:
@@ -58,9 +49,29 @@ def main():
                 break
 
     # put some data into the log
-    print(len(games))
-    print(current_unix_time)
-    print(ran)
+    print('NUM GAMES => %s' % len(games))
+    print('TIMESTAMP => %s' % current_unix_time)
+    print('LIGHTS?   => %s' % ran)
+
+
+@cached(ttl=CACHE_TTL)
+def get_schedule():
+    """Load game data from nhl.com."""
+    # load game data for the next month
+    url = 'https://statsapi.web.nhl.com/api/v1/schedule'
+    response = requests.get(url)
+    data = response.json()
+    if data['totalGames'] == 0:
+        print('No games today!')
+        exit()
+    games = data['dates'][0]['games']
+
+    # filter out postponed games, thanks COVID-19
+    games = [
+        x for x in games
+        if x.get('status', {}).get('detailedState', '') != 'Postponed'
+    ]
+    return games
 
 
 if __name__ == '__main__':
