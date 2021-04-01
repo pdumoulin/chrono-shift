ccbc2cb0386b8270d87c7a0c4c9ae2bfa33a8aff
Paul Dumoulin <paul.l.dumoulin@gmail.com>
2021-04-01 22:07:44 +0000

    update example

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # crontab setup
-# 0 0 * * * /home/pi/chrono-shift/run.sh set > /home/pi/chrono-shift/cron-set.logs 2>&1
-# * * * * * /home/pi/chrono-shift/run.sh execute> /home/pi/chrono-shift/cron-execute.logs 2>&1
+# * * * * * /home/pi/chrono-shift/run.sh execute >> /home/pi/chrono-shift/cron-execute.log 2>&1
 
 export PYTHONPATH=$PYTHONPATH:/home/pi/blinky
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
